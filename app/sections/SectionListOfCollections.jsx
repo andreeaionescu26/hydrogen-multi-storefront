37bc9e8c1c5e003b945f6b85341f6aa28ffad0e8
andreeaionescu26 <andreea26.ionescu@gmail.com>
2026-07-24 09:28:06 +0100

    fixed css and applied conistancy for max width of sections and border radius

--- app/sections/SectionListOfCollections.jsx
+++ app/sections/SectionListOfCollections.jsx
@@ -18,58 +18,61 @@ function SectionListOfCollections(props) {
 
   return (
     <section className="collection-list">
-      <div className="collection-list__top">
-        <div className="collection-list__intro">
-          {header?.value && (
-            <h2 className="collection-list__heading">{header.value}</h2>
-          )}
-          {text?.value && (
-            <div className="collection-list__text">
-              <RichText data={text.value} />
-            </div>
+      <div className="collection-list__inner">
+        <div className="collection-list__top">
+          <div className="collection-list__intro">
+            {header?.value && (
+              <h2 className="collection-list__heading">{header.value}</h2>
+            )}
+            {text?.value && (
+              <div className="collection-list__text">
+                <RichText data={text.value} />
+              </div>
+            )}
+          </div>
+
+          {hasTopButton && (
+            <Link
+              to={buttonHref}
+              prefetch="intent"
+              className="collection-list__cta"
+            >
+              {buttonLabel.value}
+            </Link>
           )}
         </div>
 
-        {hasTopButton && (
-          <Link
-            to={buttonHref}
-            prefetch="intent"
-            className="collection-list__cta"
-          >
-            {buttonLabel.value}
-          </Link>
+        {collectionNodes.length > 0 && (
+          <div className="collection-list__grid">
+            {collectionNodes.map((collection) => (
+              <Link
+                key={collection.id}
+                to={`/collections/${collection.handle}`}
+                prefetch="intent"
+                className="collection-list__card"
+              >
+                <div className="collection-list__card-media">
+                  {collection.image?.url ? (
+                    <img
+                      src={collection.image.url}
+                      alt={
+                        collection.image.altText ??
+                        displayTitle(collection.title)
+                      }
+                      loading="lazy"
+                    />
+                  ) : (
+                    <div className="collection-list__card-placeholder" />
+                  )}
+                </div>
+                <p className="collection-list__card-title">
+                  {displayTitle(collection.title)}
+                </p>
+              </Link>
+            ))}
+          </div>
         )}
       </div>
-
-      {collectionNodes.length > 0 && (
-        <div className="collection-list__grid">
-          {collectionNodes.map((collection) => (
-            <Link
-              key={collection.id}
-              to={`/collections/${collection.handle}`}
-              prefetch="intent"
-              className="collection-list__card"
-            >
-              <div className="collection-list__card-media">
-                {collection.image?.url ? (
-                  <img
-                    src={collection.image.url}
-                    alt={
-                      collection.image.altText ?? displayTitle(collection.title)
-                    }
-                    loading="lazy"
-                  />
-                ) : (
-                  <div className="collection-list__card-placeholder" />
-                )}
-              </div>
-              <p className="collection-list__card-title">
-                {displayTitle(collection.title)}
-              </p>
-            </Link>
-          ))}
-        </div>
-      )}
     </section>
   );
 }
